83894f9d4aa614714f56bfa52bb527129ab62435
unknown <ganeshbalaji160@gmail.com>
2021-07-05 11:27:35 +0530

    third

--- Python_program_basic/Python_program_basic_6/Python_program_basic_6.py
+++ Python_program_basic/Python_program_basic_6/Python_program_basic_6.py
@@ -5,6 +5,7 @@
 4.	Write a Python Program to calculate the natural logarithm of any number?
 5.	Write a Python Program for cube sum of first n natural numbers?
 """
+from math import *
 class basicprogram6:
     def question1(self,n):
         def fibo(n):
@@ -21,11 +22,19 @@ class basicprogram6:
             else:
                 return n*fact(n-1)
         return fact(n)
-    def question(self,h,w):
-        bmi = 
+    def question3(self,h,w):
+        bmi = w/(h*h)
+        return bmi
+    def question4(self,n):
+        return log10(n)
+    def question5(self,n):
+        return (n*(n+1)/2)**2
 if __name__ == "__main__":
     a6 = basicprogram6()
     print("Question 1: ")
     for i in a6.question1(10):
         print(i)
     print("Question 2:", a6.question2(5))
+    print("Question 3:", a6.question3(1.8,72))
+    print("Question 4:", a6.question4(8))
+    print("Question 5:", a6.question5(10))
